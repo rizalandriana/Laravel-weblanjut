e11f2583146abc1b9d17ab8af1fd931591a96951
rizalandriana <rizalandreana888@gmail.com>
2021-09-23 15:47:58 +0700

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // index
+    public function index(){
+        return "ini adalah index page Book";
+    }
+    public function view($judul){
+        return "ini adalah view book $judul";
+    }
+
+    // view Judul
+    /*public function viewJudul($judul){
+        return "Judul buku yang anda baca adalah".$judul;
+    }*/
+     // view Judul
+         public function viewJudul($judul){
+            $data=
+            array
+            ('judul'=>$judul);
+            return 
+            View::make('book.Book',$data);
+        }
+}
